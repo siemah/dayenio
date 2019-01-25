9e2faac73e32f683cbd9578c28d97e85389cf741
siemah <m_saidouni@inptic.edu.dz>
2019-01-25 12:25:40 +0100

    form

--- src/pages/get-in-touch.js
+++ src/pages/get-in-touch.js
@@ -69,25 +69,16 @@ export default props => {
             }
 
             <Container>
-              
-<form  method="POST" name='test-form' data-netlify="true">
+      <form name="contact" method="POST" netlify-honeypot="bot-field" data-netlify="true">
   <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
+    <label>Email: <input type="text" name="name" /></label>
   </p>
   <p>
     <label>Message: <textarea name="message"></textarea></label>
   </p>
+  <div data-netlify-recaptcha="true"></div>
   <p>
-    <button type="submit">Send</button>
+    <button type='submit'>Send</button>
   </p>
 </form>
 
