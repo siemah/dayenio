39854e8f82ce279c4d2de3cd63b6326def204c13
siemah <m_saidouni@inptic.edu.dz>
2019-02-15 01:42:57 +0100

    Update about us slider at get in touch

--- src/assets/files/about-us.js
+++ src/assets/files/about-us.js
@@ -3,21 +3,20 @@ export default [
     key: 'wellnessqatar.com',
     name: 'Mme. Reem ELTHANI',
     location: 'Doha, QATAR',
-    description: `Whether it’s front- or back-end, we can’t recommend DayenIO highly enough.\n
+    description: `Whether it’s front-end or back-end, we can’t recommend DayenIO highly enough.\n
     Over the years, they have delivered outstanding solutions for many of our brands, including 
-    Wellnessqatar  and Wellness event.`,
+    Wellnessqatar and Wellness event.`,
     post: 'CEO Wellness Inc',
   },
   {
     key: 'shahrani-m.com',
     name:'Dr. Mohammed ELSHAHRANI',
     location:'Riyad, KSA',
-    description:`Whether it’s front- or back-end, we can’t recommend Playground highly enough.
-    Over the years, they have delivered outstanding solutions for
-     many of our brands, including Dermatoline, Borotalco, Somatoline, and Rogé Cavaillès.`,
-    post : 'Doctor ',
+    description:`I am constantly impressed by the way they approach each project: no matter how 
+    crazy and urgent are the ideas and the requests we come up with, DayenIO finds solutions which always deliver.`,
+    post : 'Doctor Math',
   },
-  {
+  /*{
     key: 'mario.it',
     name: 'Mr. Mario',
     location: 'Rome, ITALY',
@@ -25,5 +24,5 @@ export default [
     Over the years, they have delivered outstanding solutions for many of our brands, including 
     Wellnessqatar  and Wellness event.`,
     post: 'Designer',
-  },
+  },*/
 ]
