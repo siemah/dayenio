49e9d1079b51fb7932cafdf493011809d7178b08
siemah <m_saidouni@inptic.edu.dz>
2019-04-13 03:57:09 +0200

    (Build)install transition page plugin to add some transition when navigate between pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -62,6 +62,13 @@ module.exports = {
         prefixDefault: false,
       }
     },
+    // transition page
+    {
+      resolve: 'gatsby-plugin-page-transitions',
+      options: {
+        transitionTime: 250
+      }
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
